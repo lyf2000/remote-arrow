11e5678b6801d885f90e58a9723ccaa6f0c5eeee
damri0110 <lyf2000@mail.ru>
2022-10-25 10:59:14 +0300

    > fix comments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
-import netifaces
+# import netifaces
 import uvicorn
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-from service import ArrowControlAdapter
+from service import ArrowControlAdapter, get_ip
 
 app = FastAPI()
 
@@ -40,12 +40,7 @@ if __name__ == "__main__":
     host = "0.0.0.0"
     port = 8000
 
-    ip_list = []
-    for interface in netifaces.interfaces():
-        if interface in ["eth0", "wlp2s0"] or interface.startswith("enp"):
-            for link in netifaces.ifaddresses(interface).get(netifaces.AF_INET, {}):
-                ip_addr = link.get("addr", None)
-                ip_list.append(ip_addr)
-    print("Try to connect to one of the following in your phone")
-    print([ip + f":{port}" for ip in ip_list], sep="\n")
+    print("Try to connect to this ip addr in your phone:")
+    ip = f"{get_ip()}:{port}"
+    print(ip)
     uvicorn.run(app, host=host, port=port)
--- service.py
+++ service.py
@@ -1,3 +1,5 @@
+import socket
+
 import pyautogui
 
 
@@ -7,3 +9,16 @@ class ArrowControlAdapter:
     @classmethod
     def click_keyb(cls, action: str):
         pyautogui.press([action])
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(("10.254.254.254", 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+    return IP
